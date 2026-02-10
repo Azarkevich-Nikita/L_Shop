7f912e6941b863ee6b6baad2e314a522f46b99b0
Daniil-Dashchenko <daniildasenko396@gmail.com>
2026-02-10 09:00:38 +0300

    feat: added types

---
+++ server/src/types/product.types.ts
@@ -0,0 +1,10 @@
+export interface Products {
+    id: number,
+    title: string,
+    price: number,
+    created_from: string,
+    is_stock: boolean,
+    weight: number,
+    compount: string[],
+    created_date: string
+}
