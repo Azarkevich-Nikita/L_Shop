31f58e8219980fd61abb1bf3991433b9379ba6b5
vsemprivet2727 <dambosssss@gmail.com>
2026-02-27 22:48:53 +0300

    add redirection if session was not found

--- client/src/components/pages/profile.tsx
+++ client/src/components/pages/profile.tsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
+    const navigate = useNavigate();
+    useEffect(() => {
+        let cancelled = false;
+
+        const fetchData = async () => {
+            try {
+                if (!cancelled) {
+                    const response = await fetch('/api/auth/me');
+                    if (response.status === 401) {
+                        navigate(confirm('Not logged in?')? '/auth' : '/');
+                        return;
+                    }
+
+                    const data = await response.json();
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            cancelled = true;
+        };
+    }, [navigate]);
+
     return(
         <>
-            <span>Profile</span>
+            Profile
         </>
     )
 }
